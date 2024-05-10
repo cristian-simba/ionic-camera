1d745871bfb9bf7af7431e47b516714e1f83c34f
cristian-simba <davidsimba200@gmail.com>
2024-05-10 15:05:13 -0500

    guardar fotos

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,8 +8,11 @@ import { PhotoService } from '../services/photo.service';
 })
 export class Tab2Page {
 
-  constructor(public photoService: PhotoService) { }
+  constructor(public photoService: PhotoService) {}
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
+  }
+  async ngOnInit(){
+    await this.photoService.loadSaved();
   }
 }
